eae19ae6e2bcbfc5bfac286f089e1e3b98dc00a5
Rachida92 <rb7592@outlook.com>
2023-04-16 23:57:27 +0200

    css ajustment only

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,10 @@ function Card({id, title, cover}){
 
     <div className='appartment-item' key={id} id={id}> 
 
+       
+
         <Link to= {`/appart/${id}`}>
+            <span className="shadow-box"></span>
             <img className='appartment-cover' src={cover} alt={`${title} cover`} />
             <span className = 'appartment-title'>{title}</span>
         </Link>
